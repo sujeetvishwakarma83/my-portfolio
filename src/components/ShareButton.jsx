96ac3d86ffdf99125bedf5ec1f97edcbf545e389
Sujeet Vishwakarma <sujeet8528420907@gmail.com>
2026-03-20 09:25:14 +0530

    update arattai share button link

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -60,7 +60,7 @@ function ShareButton({ darkMode }) {
           <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
         </svg>
       ),
-      url: 'https://arattai.in//share?url=' + encodeURIComponent(portfolioUrl) + '&title=' + encodeURIComponent(portfolioTitle),
+      url: 'https://aarattai.com/share?url=' + encodeURIComponent(portfolioUrl) + '&title=' + encodeURIComponent(portfolioTitle),
     },
     {
       name: 'Copy Link',
